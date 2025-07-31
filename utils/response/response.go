9cb4341182f8fcfe42796484fcf4ac2b8edef120
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2025-07-31 14:04:47 +0000

    add: message to response

--- utils/response/response.go
+++ utils/response/response.go
@@ -6,10 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Success(c *gin.Context, data any) {
+func Success(c *gin.Context, message string, data any) {
 	c.JSON(http.StatusOK, gin.H{
-		"status": "success",
-		"data":   data,
+		"status":  "success",
+		"message": message,
+		"data":    data,
 	})
 }
 
