80fa4915cc4911ca18203060a04a3f3d44a590ab
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2025-09-16 17:17:59 +0700

    add: config fe domain with .env

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -2,13 +2,14 @@ package middlewares
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_DOMAIN"))
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Accept")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
